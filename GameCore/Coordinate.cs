cae6ee39da171bb6a9a8acde0349ecce9b17f964
Jake Black <ablack3@harding.edu>
2018-01-16 17:53:36 -0600

    .

--- GameCore/Coordinate.cs
+++ GameCore/Coordinate.cs
@@ -3,16 +3,22 @@ namespace GameCore
 {
     public class Coordinate
     {
+
+        struct Coord
+        {
+            private int x;
+            private int y;
+        }
+ 
         public Coordinate()
         {
+            var MyCoord = new Coord();
         }
 
-        private int x;
-        private int y;
 
-        public Coordinate getCoordinate()
+        public Coord getCoordinate(Coord MyCoord)
         {
-            return new Coordinate();
+            return MyCoord;
             //This is not correct and will need to be changed come time for implementation.
         }
     }
