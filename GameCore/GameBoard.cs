521c0e9dfa8122a7724d215e90a896eb6c080ed7
Jake Black <ablack3@harding.edu>
2018-01-16 11:59:55 -0600

    Trying to Figure out c#

---
+++ GameCore/GameBoard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+System.Collections.Generic.List<>
+namespace GameCore
+{
+    public class GameBoard
+    {
+        public GameBoard()
+        {
+            
+        }
+    }
+}
